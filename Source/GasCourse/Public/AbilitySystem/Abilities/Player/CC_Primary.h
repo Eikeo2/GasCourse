0bb2a1b74bea2d1fb41b5d6261be34099b345c3f
eike <2370825366@qq.com>
2026-03-22 01:10:14 +0800

    Overlap Test

---
+++ Source/GasCourse/Public/AbilitySystem/Abilities/Player/CC_Primary.h
@@ -0,0 +1,31 @@
+﻿// Copyright eike
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/CC_GameplayAbility.h"
+#include "CC_Primary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASCOURSE_API UCC_Primary : public UCC_GameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Crash|Abilities")
+	void HitBoxOverlapTest();
+
+private:
+	//碰撞半径
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Abilities")
+	float HitBoxRadius = 100.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Abilities")
+	float HitBoxForwardOffset = 200.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Crash|Abilities")
+	float HitBoxElevationOffset = 20.0f;
+};
